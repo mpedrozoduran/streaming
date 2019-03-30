a2ad7b83473077f04121c18e7e80db4a0900a214
mpedrozoduran@gmail.com <mpedrozoduran@gmail.com>
2019-03-29 20:02:04 -0500

    Added template startup shell scripts

---
+++ streaming-app/src/main/resources/run_as_server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+DIR=`pwd`
+java -jar streaming-app-1.0-SNAPSHOT-jar-with-dependencies.jar -channelFile "$DIR/channels.txt" -mode server -port 8888 -resourceToStream /Users/mpedrozoduran/Downloads/The_Ninth_Gate_1999/The.Ninth.Gate.1999.720p.BluRay.x264.YIFY.mp4 &
